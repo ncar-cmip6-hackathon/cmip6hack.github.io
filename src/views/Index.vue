e5ad596156330c5093047d9f963435ccb8d553c0
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-05-20 14:39:05 -0600

    remove location map for the time being

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -53,18 +53,6 @@
               <h5 class="description">
                 NCAR CMIP6 Hackathon is made possible by the following sponsors:
               </h5>
-              <h2 class="title text-center">Location</h2>
-              <!--<div id="osm-map"></div>-->
-              <div id="map" width="600" height="600">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12229.39375390682!2d-105.275031!3d39.978318!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6170c343f33c8ada!2sNCAR+Mesa+Laboratory!5e0!3m2!1sen!2sus!4v1556440500223!5m2!1sen!2sus"
-                  width="500"
-                  height="450"
-                  frameborder="0"
-                  style="border:0"
-                  allowfullscreen
-                ></iframe>
-              </div>
             </div>
           </div>
         </div>
