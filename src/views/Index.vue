cbd72ace7c46e7d6003aac3cea15226f61e4da8a
mclong <mclong@ucar.edu>
2019-05-30 09:20:33 -0600

    tweaks

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -8,8 +8,9 @@
           >
             <h2 class="title">
               Dates: Oct. 16 - 18, 2019
-              <br />Venues: NCAR, Boulder, CO & <br>
-                            LDEO, Palisades, NY
+              <br />Venues: <br>
+              NCAR, Boulder, CO <br>
+              LDEO, Palisades, NY
             </h2>
             <h3>Applications are now open!</h3>
             <md-button href="https://forms.gle/CzHh9iedY1wxv9ye8" class="md-success md-lg md-round" target=_blank
@@ -28,16 +29,19 @@
             >
               <h2 class="title text-center">Overview</h2>
               <h5 class="description">
-                Centers around the world are currently running their state-of-the-art Earth system models (ESMs) to participate in the 6th phase of the Coupled Model Intercomparison Project (CMIP6), which supports the United Nations Environmental Program’s International Panel on Climate Change 6th Assessment (AR6). This event will explore new paradigms for community-driven analysis and will consist of the primary components: (1) tutorials on cutting-edge analysis tools, (2) peer-learning opportunities, and (3) open-ended project work in a highly collaborative environment.
-
-                <br>
+                Centers around the world are currently running their state-of-the-art Earth system models to participate in the 6th phase of the Coupled Model Intercomparison Project (CMIP6), which supports the United Nations Environmental Program’s International Panel on Climate Change 6th Assessment. This event will explore new paradigms for community-driven analysis and will consist of three primary components: (1) tutorials on cutting-edge analysis tools, (2) peer-learning opportunities, and (3) open-ended project work in a highly collaborative environment.
+              </h5>
+              <h5 class="description">
                 The Hackathon will be held concurrently at two locations: the
                 <a href="https://goo.gl/maps/UMQ7fzxSGiKtrKEJ9">NCAR Mesa Lab</a>, in Boulder CO, and the
                 <a href="https://goo.gl/maps/thqWXUsptr4zPCtZ6">Lamont Doherty Earth Observatory</a>, in Palisades NY.
-                <br>
+              </h5>
 
+              <h2 class="title text-center">Travel support</h2>
+              <h5 class="description">
                 Limited funding is available to support travel and lodging, with preference given to early career scientists.
-
+                Participants will be selected on the basis of interests, experience, and potential to contribute to collaborative initiatives.
+                People from observational or application-related backgrounds are encouraged to apply.
               </h5>
 
               <h2 class="title text-center">Sponsors</h2>
